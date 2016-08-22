7fefab86f659eb0a957f20c747397447770b2e9c
Paul Loyd <pavelko95@gmail.com>
2016-08-22 22:22:06 +0300

    feat(base): `Session` now is interior mutability

--- src/base/session.rs
+++ src/base/session.rs
@@ -1,5 +1,6 @@
+use std::cell::RefCell;
 use hyper::client::{Client, Response};
-use hyper::header::{Headers, SetCookie, Cookie, UserAgent};
+use hyper::header::{Headers, SetCookie, Cookie, UserAgent, ContentLength};
 use hyper::status::StatusCode;
 
 const USER_AGENT: &'static str = concat!("Mozilla/5.0 (X11; Linux x86_64) ",
@@ -8,7 +9,7 @@ const USER_AGENT: &'static str = concat!("Mozilla/5.0 (X11; Linux x86_64) ",
 pub struct Session {
     client: Client,
     base_url: String,
-    cookie: Cookie
+    cookie: RefCell<Cookie>
 }
 
 impl Session {
@@ -16,27 +17,27 @@ impl Session {
         Session {
             client: Client::new(),
             base_url: base_url.to_string(),
-            cookie: Cookie(vec![])
+            cookie: RefCell::new(Cookie(vec![]))
         }
     }
 
-    pub fn get(&mut self, path: &str) -> Response {
+    pub fn get(&self, path: &str) -> Response {
         self.request(path, None, Headers::new())
     }
 
-    pub fn get_with_headers(&mut self, path: &str, headers: Headers) -> Response {
+    pub fn get_with_headers(&self, path: &str, headers: Headers) -> Response {
         self.request(path, None, headers)
     }
 
-    pub fn post(&mut self, path: &str, body: &str) -> Response {
+    pub fn post(&self, path: &str, body: &str) -> Response {
         self.request(path, Some(body), Headers::new())
     }
 
-    pub fn post_with_headers(&mut self, path: &str, body: &str, headers: Headers) -> Response {
+    pub fn post_with_headers(&self, path: &str, body: &str, headers: Headers) -> Response {
         self.request(path, Some(body), headers)
     }
 
-    fn request(&mut self, path: &str, body: Option<&str>, mut headers: Headers) -> Response {
+    fn request(&self, path: &str, body: Option<&str>, mut headers: Headers) -> Response {
         let mut url = self.base_url.clone();
         url.push_str(path);
 
@@ -45,8 +46,12 @@ impl Session {
             None => self.client.get(&url)
         };
 
-        headers.set(self.cookie.clone());
+        headers.set(self.cookie.borrow().clone());
         headers.set(UserAgent(USER_AGENT.to_owned()));
+
+        if let Some(body) = body {
+            headers.set(ContentLength(body.len() as u64));
+        }
 
         let response = builder.headers(headers).send().unwrap();
 
@@ -55,7 +60,7 @@ impl Session {
         let cookies = response.headers.get::<SetCookie>()
             .map_or_else(Vec::new, |c| c.0.clone());
 
-        self.cookie = Cookie(cookies);
+        *self.cookie.borrow_mut() = Cookie(cookies);
 
         response
     }
