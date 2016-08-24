ece9f40749f231905be738501cb83df02c1957e2
Paul Loyd <pavelko95@gmail.com>
2016-08-24 15:21:52 +0300

    feat(opportunity): implement `Favorite` and `Rebel` strategies

--- src/opportunity.rs
+++ src/opportunity.rs
@@ -1,10 +1,12 @@
+use std::cmp::PartialOrd;
+
 use events::Outcome;
 
 use self::Strategy::*;
 
 pub enum Strategy { Unbiased, Favorite, Rebel }
 
-pub fn find_best<'a, I>(mut markets: I, strategy: Strategy) -> Option<Vec<(&'a Outcome, f64)>>
+pub fn find_best<'a, I>(markets: I, strategy: Strategy) -> Option<Vec<(&'a Outcome, f64)>>
     where I: IntoIterator<Item = &'a [Outcome]>
 {
     let mut markets = markets.into_iter();
@@ -24,7 +26,7 @@ pub fn find_best<'a, I>(mut markets: I, strategy: Strategy) -> Option<Vec<(&'a O
         }
     }
 
-    let coef = line.iter().fold(0f64, |acc, o| acc + 1./o.1);
+    let coef = line.iter().fold(0., |acc, o| acc + 1. / o.1);
 
     if coef > 1. {
         return None;
@@ -33,11 +35,28 @@ pub fn find_best<'a, I>(mut markets: I, strategy: Strategy) -> Option<Vec<(&'a O
     let result = match strategy {
         Unbiased => {
             line.iter()
-                .map(|outcome| (*outcome, 1. / line.iter().fold(0., |acc, o| acc + outcome.1/o.1)))
+                .map(|outcome| {
+                    let sum = line.iter().fold(0., |acc, o| acc + outcome.1 / o.1);
+                    (*outcome, 1. / sum)
+                })
                 .collect()
         },
+        Favorite | Rebel => {
+            let mut guess_idx = 0;
+            let cmp = if let Favorite = strategy { PartialOrd::ge } else { PartialOrd::le };
 
-        _ => unimplemented!()
+            for (idx, outcome) in line.iter().enumerate() {
+                if cmp(&outcome.1, &line[guess_idx].1) {
+                    guess_idx = idx;
+                }
+            }
+
+            let mut result = line.iter().map(|o| (*o, 1. / o.1)).collect::<Vec<_>>();
+            let sum = result.iter().fold(0., |a, o| a + o.1);
+
+            result[guess_idx].1 += 1. - sum;
+            result
+        }
     };
 
     Some(result)
@@ -51,6 +70,18 @@ macro_rules! assert_bet {
 
         assert_eq!(actual, $expected);
     }
+}
+
+#[test]
+fn test_none() {
+    let markets = [
+        [Outcome("X".to_string(), 2.3), Outcome("Y".to_string(), 1.2)],
+        [Outcome("X".to_string(), 2.1), Outcome("Y".to_string(), 1.3)]
+    ];
+
+    assert_bet!(markets, Unbiased, None);
+    assert_bet!(markets, Favorite, None);
+    assert_bet!(markets, Rebel, None);
 }
 
 #[test]
@@ -68,11 +99,29 @@ fn test_unbiased() {
 }
 
 #[test]
-fn test_unbiased_none() {
+fn test_favorite() {
     let markets = [
-        [Outcome("X".to_string(), 2.3), Outcome("Y".to_string(), 1.2)],
-        [Outcome("X".to_string(), 2.1), Outcome("Y".to_string(), 1.3)]
+        [Outcome("X".to_string(), 2.3), Outcome("Y".to_string(), 3.2)],
+        [Outcome("X".to_string(), 2.1), Outcome("Y".to_string(), 3.3)]
     ];
 
-    assert_bet!(markets, Unbiased, None);
+    let x_outcome = Outcome("X".to_string(), 2.3);
+    let y_outcome = Outcome("Y".to_string(), 3.3);
+    let expected = vec![(&x_outcome, 0.43), (&y_outcome, 0.57)];
+
+    assert_bet!(markets, Favorite, Some(expected));
+}
+
+#[test]
+fn test_rebel() {
+    let markets = [
+        [Outcome("X".to_string(), 2.3), Outcome("Y".to_string(), 3.2)],
+        [Outcome("X".to_string(), 2.1), Outcome("Y".to_string(), 3.3)]
+    ];
+
+    let x_outcome = Outcome("X".to_string(), 2.3);
+    let y_outcome = Outcome("Y".to_string(), 3.3);
+    let expected = vec![(&x_outcome, 0.7), (&y_outcome, 0.3)];
+
+    assert_bet!(markets, Rebel, Some(expected));
 }
