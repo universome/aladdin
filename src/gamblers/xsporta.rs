b5dfb1145e80b8eab88520d759927e30b0917499
Paul Loyd <pavelko95@gmail.com>
2016-09-02 19:06:17 +0300

    feat(gamblers/xsporta): decrease the period to 30 seconds

--- src/gamblers/xsporta.rs
+++ src/gamblers/xsporta.rs
@@ -35,7 +35,8 @@ impl Gambler for XBet {
         let path = "/LineFeed/Get1x2?sportId=40&count=50&cnt=10&lng=en";
         let mut map = HashMap::new();
 
-        for _ in Periodic::new(60) {
+        // The site uses 1-mitute period, but for us it's too long.
+        for _ in Periodic::new(30) {
             let message = try!(self.session.get_json::<Message>(path));
             let offers = try!(grab_offers(message));
 
