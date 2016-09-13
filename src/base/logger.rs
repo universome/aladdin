af7e93b2b79b0a7578a079458dd22af21257243e
Paul Loyd <pavelko95@gmail.com>
2016-09-13 12:37:50 +0300

    feat(base/logger): restrict size of the history

--- src/base/logger.rs
+++ src/base/logger.rs
@@ -5,6 +5,8 @@ use log::{self, Log, LogRecord, LogLevel, LogMetadata, SetLoggerError};
 use env_logger::{LogBuilder as EnvLogBuilder, Logger as EnvLogger};
 use time;
 
+use base::config::CONFIG;
+
 struct Logger(EnvLogger);
 
 impl Log for Logger {
@@ -16,10 +18,9 @@ impl Log for Logger {
         self.0.log(record);
 
         if self.enabled(record.metadata()) && record.level() <= LogLevel::Warn {
-            let mut messages = MESSAGES.write().unwrap();
             let target = record.target();
 
-            messages.push_back(Message {
+            save_to_history(Message {
                 level: record.level(),
                 module: if target.starts_with("aladdin::") {
                     target.rsplit("::").next().unwrap().to_string()
@@ -41,11 +42,23 @@ pub struct Message {
 }
 
 lazy_static! {
-    static ref MESSAGES: RwLock<VecDeque<Message>> = RwLock::new(VecDeque::new());
+    static ref HISTORY: RwLock<VecDeque<Message>> = RwLock::new(VecDeque::new());
+    static ref HISTORY_SIZE: usize = CONFIG.lookup("logging.history-size")
+        .unwrap().as_integer().unwrap() as usize;
 }
 
-pub fn acquire_messages() -> RwLockReadGuard<'static, VecDeque<Message>> {
-    MESSAGES.read().unwrap()
+fn save_to_history(message: Message) {
+    let mut history = HISTORY.write().unwrap();
+
+    if history.len() >= *HISTORY_SIZE {
+        history.pop_front();
+    }
+
+    history.push_back(message);
+}
+
+pub fn acquire_history() -> RwLockReadGuard<'static, VecDeque<Message>> {
+    HISTORY.read().unwrap()
 }
 
 pub fn init() -> Result<(), SetLoggerError> {
