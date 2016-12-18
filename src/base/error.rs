ba7187256e6d9e45bbc001cdb79f7f199e6243b4
Paul Loyd <pavelko95@gmail.com>
2016-12-18 13:52:59 +0300

    feat(base/error): involve trace using backtrace crate

--- src/base/error.rs
+++ src/base/error.rs
@@ -13,23 +13,39 @@ use serde_json::Error as JsonError;
 use hyper::status::StatusCode;
 use url::ParseError as UrlParseError;
 use websocket::result::WebSocketError;
+use backtrace::Backtrace;
 
-use self::Error::*;
+use self::ErrorKind::*;
 
 pub type BoxedError = Box<StdError + Send + Sync>;
 
 pub type Result<T> = StdResult<T, Error>;
 
 #[derive(Debug)]
-pub enum Error {
+pub struct Error {
+    pub kind: ErrorKind,
+    pub stack: Backtrace
+}
+
+#[derive(Debug)]
+pub enum ErrorKind {
     Network(BoxedError),
     Status(StatusCode),
     Unexpected(BoxedError)
 }
 
+impl Error {
+    pub fn new(kind: ErrorKind) -> Error {
+        Error {
+            kind: kind,
+            stack: Backtrace::new()
+        }
+    }
+}
+
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        match *self {
+        match self.kind {
             Network(ref e) => write!(f, "Network error: {}", e),
             Status(ref e) => write!(f, "Bad status code: {}", e),
             Unexpected(ref e) => write!(f, "Unexpected error: {}", e)
@@ -39,7 +55,7 @@ impl Display for Error {
 
 impl StdError for Error {
     fn description(&self) -> &str {
-        match *self {
+        match self.kind {
             Network(ref err) => err.description(),
             Status(ref code) => code.canonical_reason().unwrap_or("Strange status code"),
             Unexpected(ref err) => err.description()
@@ -49,16 +65,16 @@ impl StdError for Error {
 
 impl From<StatusCode> for Error {
     fn from(code: StatusCode) -> Error {
-        Status(code)
+        Error::new(Status(code))
     }
 }
 
 impl From<JsonError> for Error {
     fn from(err: JsonError) -> Error {
-        match err {
+        Error::new(match err {
             JsonError::Io(err) => Network(From::from(err)),
             err => Unexpected(From::from(err))
-        }
+        })
     }
 }
 
@@ -70,7 +86,7 @@ impl<T> From<PoisonError<T>> for Error {
 
 impl<'a> From<&'a str> for Error {
     fn from(err: &str) -> Error {
-        Unexpected(From::from(err))
+        Error::new(Unexpected(From::from(err)))
     }
 }
 
@@ -79,7 +95,7 @@ macro_rules! impl_boxed {
     ($variant:ident, $err:ty) => {
         impl From<$err> for Error {
             fn from(err: $err) -> Error {
-                $variant(From::from(err))
+                Error::new($variant(From::from(err)))
             }
         }
     }
